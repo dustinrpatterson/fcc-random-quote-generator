0adc5e57f9c68be55998dae2c911be58d9607184
Dustin Patterson <dustinrpatterson@gmail.com>
2019-02-21 14:35:50 -0700

    working and ready for submission 🔥

--- src/App.js
+++ src/App.js
@@ -2,60 +2,82 @@ import React, { Component } from "react";
 import "./App.css";
 
 class App extends Component {
-  constructor() {
+  constructor(props) {
+    super(props);
     super();
     this.getQuote = this.getQuote.bind(this);
-    this.handleClick = this.handleClick.bind(this);
     this.state = {
-      quote: null
+      quote: "loading...",
+      title: "loading...",
+      link: null
     };
   }
-  handleClick() {
-    this.getQuote();
-  }
+
   getQuote() {
+    console.log("Fired function");
     fetch(
-      "http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback="
+      "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=",
+      {
+        method: "GET", // *GET, POST, PUT, DELETE, etc.
+        mode: "cors", // no-cors, cors, *same-origin
+        cache: "no-cache" // *default, no-cache, reload, force-cache, only-if-cached
+      }
     )
       .then(results => {
-        return results.json();
+        if (results.ok) {
+          return results.json();
+        }
+        throw new Error("Network response was not ok.");
       })
       .then(data => {
         let res = data[0];
-        let quote = (
-          <div key="quote" id="quote-box">
-            <a href={res.link} rel="noopener noreferrer" target="_blank">
-              <span
-                dangerouslySetInnerHTML={{ __html: res.content }}
-                id="text"
-              />
-              <p id="author">{res.title}</p>
-            </a>
-            <button type="button" onClick={this.handleClick} id="new-quote">
-              Get new quote
-            </button>
-            <a
-              href="https://twitter.com/intent/tweet"
-              id="tweet-quote"
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              Tweet this quote
-            </a>
-          </div>
-        );
-        this.setState({ quote: quote });
-      });
+        console.log(res);
+        if (res) {
+          this.setState({
+            quote: res.content,
+            title: res.title,
+            link: res.link
+          });
+        }
+      })
+      .catch(err => console.log(err));
   }
   componentDidMount() {
     this.getQuote();
   }
   render() {
+    const { link, quote, title } = this.state;
     return (
       <div className="App">
         <h1>FCC - Random Quote Generator</h1>
         <h5>Created by Dustin Patterson</h5>
-        {this.state.quote}
+        <div key="quote" id="quote-box">
+          <div className="card">
+            <a href={link} rel="noopener noreferrer" target="_blank">
+              <span dangerouslySetInnerHTML={{ __html: quote }} id="text" />
+            </a>
+            <p id="author"> - {title}</p>
+            <a
+              href={`https://twitter.com/intent/tweet?text=${quote.replace(
+                /<[^>]+>/g,
+                ""
+              )}%20-%20${title}`}
+              id="tweet-quote"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              <i class="fab fa-twitter fa-2x" />
+            </a>
+          </div>
+          <button
+            type="button"
+            onClick={this.getQuote}
+            id="new-quote"
+            class="btn btn-primary"
+          >
+            Get new quote
+          </button>
+        </div>
       </div>
     );
   }
